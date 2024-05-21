88eea6abc8d9b470586e4ef4eac580a8053ec2ed
t1gerstarr <167377803+t1gerstarr@users.noreply.github.com>
2024-05-22 11:39:15 +1200

    Movement Animations
    I added the animations for movement to the player when moving. I ran into some bugs that I am trying to work out where my player will flip on the x axis well but has trouble on the Y. It flips but goes backwards.

--- Underwater Rougelike/Assets/Scripts/Player.cs
+++ Underwater Rougelike/Assets/Scripts/Player.cs
@@ -5,20 +5,34 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 10f;
-
-    Vector2 moveInput;
-
+    Animator animator; // Accesses the Animator component for my player
+    [SerializeField] float movementSpeed = 10f; // Sets the default movement speed of the player
+    SpriteRenderer playerSprite;
+    Vector2 moveInput; // Gets the vector2 (movement on a 2D plane) and assigns it to the moveInput variable
+    private Vector2 previousPosition;
     Rigidbody2D myRigidBody2D; // Making a variable for the players Rigidbody
+
+    bool movingHorizontal;
+    bool movingVertical;
     
     void Start()
     {
-        myRigidBody2D = GetComponent<Rigidbody2D>(); // Connecting the variable to the Rigidbody component
+        myRigidBody2D = GetComponent<Rigidbody2D>(); // Initialises the Rigidbody2D variable
+        animator = GetComponent<Animator>(); // Initialises the Animator variable
+        previousPosition = transform.position; // Initialises the previousPosition with the inital position of player
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Movement();
+        FlipSprite();
+        Vector2 currentPosition = transform.position;
+
+        movingHorizontal = Mathf.Abs(currentPosition.x - previousPosition.x) > Mathf.Epsilon;
+        movingVertical = Mathf.Abs(currentPosition.y - previousPosition.y) > Mathf.Epsilon;
+
+        previousPosition = currentPosition;
     }
 
     void OnMove(InputValue value)
@@ -30,5 +44,29 @@ public class Player : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * movementSpeed, moveInput.y * movementSpeed);
         myRigidBody2D.velocity = playerVelocity;
+
+        animator.SetBool("isMovingH", movingHorizontal);
+        animator.SetBool("isMovingV", movingVertical);
+    }
+
+    void FlipSprite()
+    {
+        if (moveInput.x > 0)
+        {
+            playerSprite.flipX = false;
+        }
+        else if (moveInput.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+
+        if (moveInput.y < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }

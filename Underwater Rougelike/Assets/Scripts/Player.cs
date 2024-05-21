a23cfb107abead41a077b7cdd028a1ffc4ab5bc6
t1gerstarr <167377803+t1gerstarr@users.noreply.github.com>
2024-05-21 15:05:15 +1200

    Basic Movement
    I added some basic up/down and lef/right movement.

--- Underwater Rougelike/Assets/Scripts/Player.cs
+++ Underwater Rougelike/Assets/Scripts/Player.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 10f;
+
+    Vector2 moveInput;
+
+    Rigidbody2D myRigidBody2D; // Making a variable for the players Rigidbody
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody2D = GetComponent<Rigidbody2D>(); // Connecting the variable to the Rigidbody component
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Movement()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * movementSpeed, moveInput.y * movementSpeed);
+        myRigidBody2D.velocity = playerVelocity;
     }
 }

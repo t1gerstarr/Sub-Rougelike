4b2d92374c549d7a131d3900a107ffb528bcdcf6
t1gerstarr <167377803+t1gerstarr@users.noreply.github.com>
2024-05-30 10:42:39 +1200

    Enemy Movement done
    Finished enemy movement (at least for one enemy) but all should follow the same formula / have the same script.

--- Underwater Rougelike/Assets/Scripts/Enemies.cs
+++ Underwater Rougelike/Assets/Scripts/Enemies.cs
@@ -9,6 +9,8 @@ public class Enemies : MonoBehaviour
     public float speed;
     public float distanceToFollow;
     public float distanceToAttack;
+    [SerializeField] float seperationRadius; // min distance between enemies
+    [SerializeField] float seperationStrength; // strength of the force pushing them away from each other
 
     // Private variables to use on ALL enemies
     private float distance;
@@ -44,8 +46,38 @@ public class Enemies : MonoBehaviour
         if(distance < distanceToFollow && distance > distanceToAttack)
         {
             animator.SetBool("isMoving", true);
-            myRigidBody.velocity = direction * speed;
+            
+            // seperation between enemies
+            Vector2 seperationForce = Vector2.zero;
+            GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("ExploFish");
+            foreach (GameObject enemy in allEnemies)
+            {
+                if (enemy != gameObject)
+                {
+                    Vector2 toOtherEnemy = transform.position - enemy.transform.position;
+                    float distanceToOtherEnemy = toOtherEnemy.magnitude;
 
+                    if (distanceToOtherEnemy > 0 && distanceToOtherEnemy < seperationRadius)
+                    {
+                        seperationForce += toOtherEnemy.normalized / distanceToOtherEnemy;
+                    }
+                }
+            }
+
+            // Combine movement towards player and seperation force
+            Vector2 combinedForce = direction * speed + seperationForce * seperationStrength;
+
+            // making sure it is not divided by 0
+            if (!float.IsNaN(combinedForce.x) && !float.IsNaN(combinedForce.y))
+            {
+                myRigidBody.velocity = combinedForce;
+            }
+            else
+            {
+                myRigidBody.velocity = Vector2.zero;
+            }
+
+            // Flip enemy sprite
             if (direction.x < 0)
             {
                 spriterenderer.flipX = true;
